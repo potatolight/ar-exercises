3b0209bf92ced71225486d6494d58b6b07dc2c0d
potatolight <shuguangbai@gmail.com>
2020-09-15 01:38:21 +0000

    average methods, total methods

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -11,4 +11,9 @@ store4 = Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: tr
 store5 = Store.create(name: "Whistler", annual_revenue: 1900000, womens_apparel: false, mens_apparel: false)
 store6 = Store.create(name: "Yaletown", annual_revenue: 430000, womens_apparel: true, mens_apparel: true)
 
-# puts Store.count
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each{|store| puts "name: #{store[:name]} and revenue: #{store[:annual_revenue]}"}
+
+
+@womens_stores = Store.where(womens_apparel: true).where("annual_revenue < 1000000")
+@womens_stores.each{|store| puts "name: #{store[:name]} and revenue: #{store[:annual_revenue]}"}
