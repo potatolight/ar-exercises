b0622dc4d47da453fc8765425544824028263306
potatolight <shuguangbai@gmail.com>
2020-09-15 00:44:24 +0000

    add, delete and update

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,8 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+store1 = Store.create(name: "Burnaby", annual_revenue: 300000, womens_apparel: true, mens_apparel: true)
+store2 = Store.create(name: "Richmond", annual_revenue: 1260000, womens_apparel: true, mens_apparel: false)
+store3 = Store.create(name: "Gastown", annual_revenue: 190000, womens_apparel: false, mens_apparel: true)
+
+ puts "show the count #{Store.count}"
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,8 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store4 = Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: true, mens_apparel: false)
+store5 = Store.create(name: "Whistler", annual_revenue: 1900000, womens_apparel: false, mens_apparel: false)
+store6 = Store.create(name: "Yaletown", annual_revenue: 430000, womens_apparel: true, mens_apparel: true)
+
+# puts Store.count
